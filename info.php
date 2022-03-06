6fbdd2f732701969d427c750c5635f85c1d53e98
mmdbest <101012556+mmdbest@users.noreply.github.com>
2022-03-07 00:48:55 +0300

    Add files via upload

---
+++ info.php
@@ -0,0 +1,10 @@
+<?php 
+
+$token="5230240313:AAF8lbWXhk5h7eV04bZUP8MkO7L9sUyfkNA";//توکن رباتی که میخوای بیاد بالا
+
+$id=0;
+$apikey="AIzaSyC7QclQ9Xm9HFseWTnpsxR5dnPoqvFARdQ";//Server Key فایربیس
+
+$admin_list=[1944893359];//ایدی عددی خودت یا گروهی که ربات توش ادمینه و میخوای بیاد بالا
+
+?>
